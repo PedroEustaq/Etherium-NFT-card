d10a25d7d58a64f8e12ad2d1501e3c4161130f72
Pedro Eustáquio <pedroeustaquioassad@gmail.com>
2024-08-09 12:31:20 -0300

    upda

---
+++ script.js
@@ -0,0 +1,10 @@
+var olho = document.getElementById('view')
+var cia = document.getElementById('cia')
+function mouseO() {
+olho.style.opacity = "1";
+cia.style.opacity = "0.7";
+}
+function mouseL() {
+    olho.style.opacity = '0';
+    cia.style.opacity = "0";
+}
